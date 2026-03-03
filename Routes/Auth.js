4f43a7487969c5ce7a9ddfebb8276b381e2f1483
ih3421 <ismail.hussain2302@gmail.com>
2026-03-03 20:28:17 +0530

    Refactor User import to use destructuring

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('./models');
+const {User} = require('./models');
 const router = express.Router();
 
 const generateToken = (user) => {
