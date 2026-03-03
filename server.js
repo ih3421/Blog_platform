932ba393e5194d602b54d66a12b0a774e8758a2f
ih3421 <ismail.hussain2302@gmail.com>
2026-03-03 20:22:31 +0530

    Uncomment authentication and post routes

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const helmet = require('helmet'); // For improving security
 const morgan = require('morgan'); // For logging HTTP requests received by the server
 require('dotenv').config();       // Configures a .env file 
 
-/*const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/posts');
-const sequelize = require('./models');*/
+const sequelize = require('./models');
 
 const app = express();
 const PORT = process.env.port;  // change port from .env file
@@ -18,15 +18,15 @@ app.use(express.json()); // to parse json
 app.use(morgan('dev'));  //logs HTTP requests in dev friendly way
 
 // Routes
-/*app.use('/api/auth', authRoutes);
-app.use('/api/posts', postRoutes);*/
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
 
 // Health check
 app.get('/health', (req, res) => res.json({ status: 'OK' })); // for clients to check if server is working or not
 
 async function startServer() {
   try {
-    await sequelize.authenticate();
+    await sequelize.authenticate();  //Check database connection
     console.log('Database connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
